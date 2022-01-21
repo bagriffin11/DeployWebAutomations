d133fe1dee8b2e26f7ca0c4710c343ad0510a10b
bagriffin11 <48770963+bagriffin11@users.noreply.github.com>
2022-01-21 15:29:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const {validateToken} = require("./jwt/Jwt")
 require('dotenv').config()
+app.set('trust proxy', 1)
 
 app.use(express.json()); //allows for json to be used in insomnia 
 app.use(cors({
